aa78b284749f00149d44c54f39ab4e50017f9b5d
TooKayAway <nhuff1@mail.csuchico.edu>
2018-02-06 11:16:34 -0800

    takes 2 numbers, then tells the largest.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,35 @@
-//Author:
+//Author: Noah Huff
+
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int number1;
+
+  int number2;
+   
   cout<<"Please enter a whole number:\n";
   
+  std::cin >> number1;
+
   cout<<"Please enter another whole number:\n";
 
+  std::cin >> number2;
+
   cout<<"Of those two numbers, the biggest is: ";
+  
+  if ( number1 > number2 ) {
+
+    std::cout << number1 << std::endl;
+
+  } else {
+
+    std::cout << number2 << std::endl;
+
+  }
+
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
